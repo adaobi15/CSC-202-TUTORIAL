b00a7ed1e357db2a0001977b305ea2b772caebe1
adaobi15 <93054208+adaobi15@users.noreply.github.com>
2023-04-15 17:26:14 +0100

    message

---
+++ comments/2. variables/exercise.js
@@ -0,0 +1,10 @@
+let pluralNoun = "roses";
+let pluralNoun2 = "violets";
+let singluarNoun = "I";
+let singluarNoun2 = "YOU";
+let color = "blue";
+let color2 =" red";
+
+console.log (pluralNoun  +  " are "  + color)
+console.log (pluralNoun2 + " are " + color2)
+console.log (singluarNoun + " love " + singluarNoun2)
